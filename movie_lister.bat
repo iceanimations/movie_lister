07fc0279083d2f114644a7426b25b53c1eefde78
Talha Ahmed <talha.ahmed@gmail.com>
2017-10-16 12:30:13 +0500

    abs path for py file

--- movie_lister.bat
+++ movie_lister.bat
@@ -4,4 +4,4 @@ setlocal
 set PATH=R:\Pipe_Repo\Users\Qurban\applications\ffmpeg\bin;%PATH%
 set PATH=R:\Pipe_Repo\Users\Qurban\applications\Python27;%PATH%
 
-python movie_lister.py %*
+python R:\Pipe_Repo\Users\Qurban\applications\runables\movie_lister\movie_lister.py %*
